67e4ed9d455faa3148cd283bbae25ae6496449a7
chirag gandhi <chirag.gandhi@tothenew.com>
2021-09-14 22:13:15 +0530

    fixed projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,12 +7,9 @@ import "../../style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import covid from "../../Assets/Projects/covid19.jpg";
-import bistro from "../../Assets/Projects/bistro.png";
+import buzzz from "../../Assets/Projects/buzzz.png";
 import chatapp from "../../Assets/Projects/chatapp.png";
 import twitter from "../../Assets/Projects/twitter.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import algo from "../../Assets/Projects/algo.png";
-import plant from "../../Assets/Projects/plant.jpeg";
 
 function Projects() {
   return (
@@ -26,26 +23,21 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={covid}
-              isBlog={false}
-              title="Covid-19 Detection"
-              description="Detecting Covid-19 along with other respiratory diseases like pneumonia using the siamese network.
-              It involved creating a siamese resnet architecture and to train it from scratch to predict accurate results for 4 classes.
-              Covid, SevereCovid, Normal, Pneumonia"
-              link="#"
-            />
-          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bistro}
+              imgPath={buzzz}
               isBlog={false}
-              title="BISTRO"
-              description="A responsive resturant application completely build on react-framework and react-router for routing. The menu can be modified by just changing the 
-              realtime-database Firebase."
-              link="https://nostalgic-heyrovsky-9c3bd1.netlify.app/#/#/"
+              title="Buzzz"
+              description="
+              A social media application completely build on MERN stack.
+              It has all the basic functionalites like
+              add/remove friend, 
+              adding/commenting/deleting/like/dislike posts,
+              It also has a moderator mode.
+              "
+              github_link="https://github.com/chirag-ttn/PROJECT_BACKEND"
+              link="https://buzzz-1.herokuapp.com/"
             />
           </Col>
 
@@ -56,6 +48,7 @@ function Projects() {
               title="ChatApp"
               description="A real-time chat application build on above express and socket.io framework for peer to peer communication.
               The application is deployed on heroku."
+              github_link="https://github.com/chiraggandhi123/my-discord-clone"
               link="http://my-discord-clone.herokuapp.com/"
             />
           </Col>
@@ -66,9 +59,21 @@ function Projects() {
               isBlog={false}
               title="Twitter-Clone"
               description="Created a fronted twitter clone, build entirely with the modern frameworks like react.js. The routes are managed through react-router. All the data is fetched thorugh Firebase real-time database."
+              github_link = "https://github.com/chiraggandhi123/fullstack-twitter-clone"
               link="https://my-twitter-clone-v2.herokuapp.com/login"
             />
           </Col>
+        <Col md={4} className="project-card">
+          <ProjectCard
+            imgPath={covid}
+            isBlog={false}
+            title="Covid-19 Detection"
+            description="Detecting Covid-19 along with other respiratory diseases like pneumonia using the siamese network.
+            It involved creating a siamese resnet architecture and to train it from scratch to predict accurate results for 4 classes.
+            Covid, SevereCovid, Normal, Pneumonia"
+            link="#"
+          />
+        </Col>
 
           
         </Row>
